7ae37f35f9bf2cc091273c065378fc82bb40f39f
chaves <chavesprogrammer@gmail.com>
2025-06-25 17:27:05 -0300

    add slogan

---
+++ src/components/Slogan.jsx
@@ -0,0 +1,14 @@
+
+const Slogan = () => {
+  return (
+    <div className="w-full max-w-xs md:max-w-xl lg:max-w-6xl flex flex-col mx-auto text-center justify-center px-4 py-8 gap-4 ">
+        <h1 className="font-Logo text-6xl md:text-8xl font-semi mb-2">Benefix</h1>
+        <h2 className="text-2xl md:text-4xl mb-2">Conectando Empresas, Simplificando Benefícios.</h2>
+        <h3 className="text-base md:text-lg text-gray-700">
+            Plataforma completa para administração de benefícios, parcerias estratégicas e suporte inteligente via IA.
+        </h3>
+    </div>
+  )
+}
+
+export default Slogan
