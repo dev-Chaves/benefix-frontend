cb0a15a215e92b66f8eebffaa929037c5f465cab
chaves <chavesprogrammer@gmail.com>
2025-06-25 17:27:52 -0300

    add slogan

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,11 @@
 import NavBar from "../components/NavBar"
 import Hero from "../components/Hero"
+import Slogan from "../components/Slogan"
 const LandingPage = () => {
   return (
-    <div className="bg-background h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-background">
       <NavBar/>
+      <Slogan/>
       <Hero/>
     </div>
   )
